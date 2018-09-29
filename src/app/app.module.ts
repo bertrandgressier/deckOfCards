8f67a7bef58e368da2c4dd8b49f4e052c9d02f34
Bertrand Gressier <bertrand.gressier@gmail.com>
2018-09-29 19:23:06 -0400

    feat: create game table UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/root/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatToolbarModule} from '@angular/material';
+import { CardComponent } from './components/card/card.component';
+import { CardTableComponent } from './components/card-table/card-table.component';
+import { CardMenuComponent } from './components/card-menu/card-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardTableComponent,
+    CardMenuComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
